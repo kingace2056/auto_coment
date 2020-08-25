8aeff5c80c55c42e418fd5f8e4eef64d23d14787
kingace2056 <46929074+kingace2056@users.noreply.github.com>
2020-08-25 19:04:24 +0545

    Random comment generator feature added .
    You can add random comments in the list and you will be able to spam comments without timeout or getting banned .

--- autocmt.py
+++ autocmt.py
@@ -1,5 +1,6 @@
 import pyautogui
 import time
+import random
 from pynput.mouse import Button,Controller 
 mouse = Controller()
 
@@ -11,18 +12,23 @@ mouse = Controller()
 # a=mouse.position
 # print(a)
 
-#dont forget to comment these lines while finding out 
-# change values in range to specify number of comments to be done
-for i in range(5):
-    mouse.position = (847,303)
-    #alter values in sleep for desired delay in comments . 
+####dont forget to comment these lines while finding out 
+###change values in range to specify number of comments to be done
+
+
+
+for i in range(20):
+    cmtList = ['Goli OP <3 <3 ','Goli Le hanxa hai aaba','Hello guys','Goli ko supporters le Goli OP lekham hai','Dont Spam Hai Guys','Revival Gaming OP','Revival OP','Dai audience alli kam vaye hai ', 'Goli OP <3 <3 ']
+    mouse.position = (851, 256)
+    #alter values in sleep for desired delay in comments . I prefer keeping all of them 2 to avoid getting banned .
     time.sleep(1)
     pyautogui.click()
     #insert your comments here 
-    pyautogui.typewrite('instagram.com/fiverrnp')
+    pyautogui.typewrite(random.choice(cmtList))
+    # pyautogui.typewrite("Goli op <3 <3  ECN OP <3 <3 <3 ")
     time.sleep(1)
-    mouse.position = (858, 351)
-    time.sleep(3)
+    mouse.position = (855, 309)
+    time.sleep(1)
     pyautogui.click()
     time.sleep(1)
     
